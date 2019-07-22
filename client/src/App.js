0b8f49e6ed0a7fc9852c51510c9d5cb20acbb64a
Austin Blaine <theaustinblaine@gmail.com>
2019-07-22 15:12:59 -0400

    Header is in place, Beginning work on models list

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import SingleBrand from './components/SingleBrand'
+import Header from './components/Header.jsx'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <Header />
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/brands/:brandId" component={SingleBrand}/>
